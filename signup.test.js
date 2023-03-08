09652743575c6edb6dbc3c4e74be376be853f5ef
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-03-08 22:47:04 +0200

    Update signup.test.js

--- signup.test.js
+++ signup.test.js
@@ -5,14 +5,17 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 
 const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.set('strictQuery', true);
+
+let server;
 
 beforeAll(async () => {
   await mongoose
     .connect(DB_HOST)
     .then(() => {
       console.log('Database connect success');
-      app.listen(PORT, () => {
+      server = app.listen(PORT, () => {
         console.log('Server running. Use our API on port: 3000');
       });
     })
@@ -24,10 +27,9 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await mongoose.disconnect();
+  await server.close();
   console.log('Database disconnect');
 });
-
-const { logIn } = require('./controllers/auth');
 
 describe('Database connection', () => {
   it('should connect to the database successfully', done => {
@@ -49,22 +51,16 @@ describe('Database connection', () => {
 describe('logIn', () => {
   it('should return a token and user object when valid credentials are provided', async () => {
     const req = { body: { email: 'test@example.com', password: 'password' } };
-    const res = { status: jest.fn().mockReturnThis(), json: jest.fn() };
-
-    await request(app)
-      .post('/login')
+    const res = await request(app)
+      .post('/api/users/login')
       .send(req.body)
       .expect(200)
-      .expect('Content-Type', /json/)
-      .expect({
-        token: expect.any(String),
-        user: { email: 'test@example.com', subscription: 'starter' },
-      });
+      .expect('Content-Type', /json/);
 
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({
-      token: expect.any(String),
-      user: { email: 'test@example.com', subscription: 'starter' },
+    expect(res.body.token).toEqual(expect.any(String));
+    expect(res.body.user).toEqual({
+      email: 'test@example.com',
+      subscription: 'starter',
     });
   });
 
@@ -72,12 +68,11 @@ describe('logIn', () => {
     const req = {
       body: { email: 'test@example.com', password: 'wrongpassword' },
     };
-
     await request(app)
-      .post('/login')
+      .post('/api/users/login')
       .send(req.body)
       .expect(401)
       .expect('Content-Type', /json/)
-      .expect({ error: 'Email or password is wrong' });
+      .expect({ message: 'Email or password is wrong' });
   });
 });
